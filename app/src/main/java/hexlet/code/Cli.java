25200b2781277bd8dc89c3282c6a6d333f781714
Sanapol <junrey@list.ru>
2024-08-21 16:39:13 +0300

    add Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+class Cli {
+    public static void userName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        var name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+        }
