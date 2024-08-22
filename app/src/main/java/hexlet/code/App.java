eee92f858b1799e7ee5c18906c35e987e1122769
Sanapol <junrey@list.ru>
2024-08-22 16:32:44 +0300

    update App.java, Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.userName();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter." +
+                "\n2 - Even" +
+                "\n1 - Greet" +
+                "\n0 - Exit");
+        var choice = scanner.nextInt();
+        if (choice == 0) {
+            System.exit(0);
+        } else if (choice == 1) {
+            Cli.greeting();
+        } else if (choice == 2) {
+            Even.gameEven();
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 class Cli {
-    public static void userName() {
+    public static void greeting() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
-        var name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
+        var userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
     }
 }
