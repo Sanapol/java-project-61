b298eed79d19c5a9d4ca8425b1a05d78095c2366
Sanapol <junrey@list.ru>
2024-08-26 16:33:48 +0300

    update App.java, Even.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,16 +6,25 @@ class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter."
+                + "\n3 - Calc"
                 + "\n2 - Even"
                 + "\n1 - Greet"
                 + "\n0 - Exit");
         var choice = scanner.nextInt();
-        if (choice == 0) {
-            System.exit(0);
-        } else if (choice == 1) {
-            Cli.greeting();
-        } else if (choice == 2) {
-            Even.gameEven();
+        switch (choice) {
+            case 0:
+                return;
+            case 1:
+                Cli.greeting();
+                break;
+            case 2:
+                Engine.run(1);
+                break;
+            case 3:
+                Engine.run(2);
+                break;
+            default:
+                throw new RuntimeException("wrong operations");
         }
     }
 }
--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,44 +1,14 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Even {
-    public static void gameEven() {
-        Scanner scanner = new Scanner(System.in);
-        var numberOfResponses = 0;
-
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        var userName = scanner.nextLine();
-        System.out.println("Hello, " + userName + "!");
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-        for (var i = 1; i <= 3; i++) {
+    public static String[][] gameRun() {
+        String[][] questionAnswer = new String[3][2];
+        for (var i = 0; i < 3; i++) {
             int random = (int) (Math.random() * 100);
-            System.out.println("Question: " + random + "\nYour answer: ");
-            var answer = scanner.nextLine();
-            if (random % 2 == 0) {
-                if (answer.equalsIgnoreCase("yes")) {
-                    System.out.println("Correct!");
-                    numberOfResponses += 1;
-                } else {
-                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'yes'."
-                            + "\nLet's try again, " + userName + "!");
-                    break;
-                }
-            } else {
-                if (answer.equalsIgnoreCase("no")) {
-                    System.out.println("Correct!");
-                    numberOfResponses += 1;
-                } else {
-                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'yes'."
-                            + "\nLet's try again, " + userName + "!");
-                    break;
-                }
-            }
+            var answer = random % 2 == 0 ? "yes" : "no";
+            questionAnswer[i][0] = (String.valueOf(random));
+            questionAnswer[i][1] = answer;
         }
-        if (numberOfResponses == 3) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
+        return questionAnswer;
     }
 }
