6cd774626e11736be7a124ea46bc2f2ef0799435
Sanapol <junrey@list.ru>
2024-08-27 03:42:31 +0300

    add Prime.java, Constants.java

---
+++ app/src/main/java/hexlet/code/Constants.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+public class Constants {
+    public static final int NUMBER_OF_ROUNDS = 3;
+    public static final int MAX_VALUE = 100;
+    public static final int ONE_ROUND = 2;
+    public static final int MIN_VALUE = 1;
+    public static final int MIN_VALUE_SIZE = 5;
+    public static final int MAX_VALUE_SIZE = 6;
+    public static final int MAX_VALUE_STEP = 10;
+    public static final int MAX_VALUE_OPERATIONS = 3;
+}
---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,31 @@
+package hexlet.code.games;
+
+import hexlet.code.Constants;
+
+public class Prime {
+    public static String[][] gameRun() {
+        var questionAnswer = new String[Constants.NUMBER_OF_ROUNDS][Constants.ONE_ROUND];
+        for (var i = 0; i < Constants.NUMBER_OF_ROUNDS; i++) {
+            int number = (int) (Math.random() * Constants.MAX_VALUE);
+            questionAnswer[i][0] = generateQuestion(number);
+            questionAnswer[i][1] = generateAnswer(number);
+        }
+        return questionAnswer;
+    }
+
+    public static String generateQuestion(int number) {
+        var question = String.valueOf(number);
+        return question;
+    }
+
+    public static String generateAnswer(int number) {
+        var halfNumber = number / 2;
+
+        for (var i = 2; i <= halfNumber; i++) {
+            if (number % i == 0) {
+                return "no";
+            }
+        }
+        return "yes";
+    }
+}
